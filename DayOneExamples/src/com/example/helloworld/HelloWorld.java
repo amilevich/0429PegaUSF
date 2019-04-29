f7c3b78ee2225620b069b9f52c04ea3482203347
amilevich <alexandra.i.milevich@gmail.com>
2019-04-29 16:24:36 -0400

    first day examples

---
+++ DayOneExamples/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+
+	// this is a single line comment 
+	/*
+	 * this is a multiline comment woo java is 
+	 * awesome
+	 */
+	
+	/**
+	 * this is javadocs
+	 * @param args 
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, guyzz");
+	}
+	 
+	/*
+	 * Java naming conventions
+	 * variables - camelCase, pascalCase
+	 * methods - camelCase, generally a verb
+	 * classes - TitleCase, named after nouns
+	 * interfaces - TitleCase, named after adjectives
+	 * packages - lowercase
+	 */
+
+}
