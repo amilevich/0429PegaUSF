7342e67d6a3882564ff5ad5c7c610e8f495d2cd0
chandaravuth-non <chandaravuth.non@gmail.com>
2019-05-01 11:16:28 -0400

    upload Question number 2

---
+++ Chandaravuth_Non_Code/Q2.java
@@ -0,0 +1,29 @@
+package test1;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class Q2 {
+	
+	
+	public static void main(String args[]) {
+		Q2 q = new Q2();
+		
+		System.out.println(q.fibonacci(6));
+		
+	}
+	
+	public  int fibonacci(int n) {
+		int result;
+		if(n==0) {
+			return 0;
+		}else if(n==1) {
+			return 1;
+		}else {
+			result = fibonacci(n-1)+fibonacci(n-2);
+		}
+		
+		
+		return result;
+	}
+
+}
