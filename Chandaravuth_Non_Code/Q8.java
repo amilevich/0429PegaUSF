f5de2c23f6c5fbf78bd19d2c6d95cb859e6d0c90
chandaravuth-non <chandaravuth.non@gmail.com>
2019-05-02 09:43:00 -0400

    add question number 8 without add commend on it. edit it later

---
+++ Chandaravuth_Non_Code/Q8.java
@@ -0,0 +1,34 @@
+package test1;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Q8 {
+
+	public static void main(String[] args) {
+	String [] s1 = {"karan","madam","tom","civic","radar","sexes","jimmy","kayak"
+					,"john","refer","billy","did"};
+	
+	arrayListMethod(s1);
+	}
+	
+	public static void arrayListMethod(String [] s)
+	{
+		ArrayList<String> Array1 = new ArrayList<>();
+		ArrayList<String> Array2 = new ArrayList<>();
+		
+		for(int i=0;i<s.length;i++)
+		{
+			Array1.add(s[i]);
+		}
+		System.out.println("ArrayList1:  ");
+		System.out.println(Array1);
+		System.out.println();
+		
+		System.out.println("ArrayList2 Copy from ArrayList1:  ");
+		Array2.addAll(Array1);
+		System.out.println(Array2);
+	}
+
+}
