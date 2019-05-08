62229a4f4985902948842ff387416b6be12ec8db
Omo-Odunjo <oodunjo28@gmail.com>
2019-05-08 02:03:09 -0400

    Project Zero - Odunjo

---
+++ JAVA_Bank_Application-master/src/com/company/bankFunctionality/IONotEnoughException.java
@@ -0,0 +1,9 @@
+package com.company.bankFunctionality;
+
+public class IONotEnoughException extends Exception {
+
+    public IONotEnoughException(String message)
+    {
+        super(message);
+    }
+}
